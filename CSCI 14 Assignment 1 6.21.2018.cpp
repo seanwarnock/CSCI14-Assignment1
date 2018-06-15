0d8c40c21b3fb5cf9e22745c386d7274b92bac01
Sean Warnock <swarnock@warnocksolutions.com>
2018-06-14 22:56:29 -0700

    Initial commit

---
+++ CSCI 14 Assignment 1 6.21.2018.cpp
@@ -0,0 +1,36 @@
+/*
+Sean Warnock
+CSCI 14 Summer 2018
+Assignment 1 due 6/21/2018
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  /*
+  whizbags    5.00
+  gismos      10.15
+  widgets     5.99
+  wabbits     7.75
+  elmers      9.50
+
+
+  */
+
+//Local variables
+  char  CustomerID[10];
+  float ShippingRate = .1;
+  float SalesTaxRate = .095;
+
+
+//*********************************************************************************************
+
+  cout << "Hello World";
+  cin;
+  cout << "Hello World";
+  return 0; //Yes this is here so it does not show as an error condition on program termination.
+}

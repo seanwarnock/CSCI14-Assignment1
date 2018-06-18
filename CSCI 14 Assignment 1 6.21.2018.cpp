fbbd216b25673462f282f2299e646b455cd0efa2
Sean Warnock <swarnock@warnocksolutions.com>
2018-06-17 23:01:24 -0700

    Whole bunch of changes
    Base menu idea working...

--- CSCI 14 Assignment 1 6.21.2018.cpp
+++ CSCI 14 Assignment 1 6.21.2018.cpp
@@ -47,37 +47,58 @@ int main()
     cout << "Account Number:\n";
     cin >> CustomerID;
 
-    cout << "[1] Whizbangs " <<  5.00 << "\n";
-    cout << "[2] Gizmos " <<    10.15 << "\n";
-    cout << "[3] Widgets " <<   5.99 << "\n";
-    cout << "[4] Wabbits " <<   7.75 << "\n";
-    cout << "[5] Elmers " << 9.50 << "\n";
 
-      do
-      {
-        cin >> intShit;
-        switch (intShit)
-          {
-              case 1 : cout << "Whizbang";
-              break;
-              case 2 : cout << "Gizmo";
-              break;
-              case 3 : cout << "Widget";
-              break;
-              case 4 : cout << "Wabbit";
-              break;
-              case 5 : cout << "Elmers";
-              break;
-          }
+
+    do
+    {
+      system("cls");
+      cout << "What would you like today?\n";
+      cout << "[1] Whizbangs " <<  5.00 << "\n";
+      cout << "[2] Gizmos " <<    10.15 << "\n";
+      cout << "[3] Widgets " <<   5.99 << "\n";
+      cout << "[4] Wabbits " <<   7.75 << "\n";
+      cout << "[5] Elmers " << 9.50 << "\n";
+      cout << "[9] Quit\n";
+      cin >> intShit;
+
+      switch (intShit)
+        {
+          case 1 : cout << "\nWhizbang quantity to order?";
+          cin >> intQty;
+          Whizbangs = Whizbangs + intQty;
+          break;
+          case 2 : cout << "\nGizmo quantity to order?";
+          cin >> intQty;
+          Gizmos = Gizmos + intQty;
+          break;
+          case 3 : cout << "\nWidget quantity to order?";
+          cin >> intQty;
+          Widgets = Widgets + intQty;
+          break;
+          case 4 : cout << "\nWabbit quantity to order?";
+          cin >> intQty;
+          Wabbits = Wabbits + intQty;
+          break;
+          case 5 : cout << "\nElmers quantity to order?";
+          cin >> intQty;
+          Elmers = Elmers + intQty;
+          break;
+          case 9 : intQty = 0;
+          break;
+        }
       } while (intQty !=0 );
 
-
-
+    system("cls");
+    cout << "Item - Qty";
+    cout << "Whizbangs " << Whizbangs << "\n";
+    cout << "Gizmos " << Gizmos << "\n";
+    cout << "Widgets " << Widgets << "\n";
+    cout << "Wabbits " << Wabbits << "\n";
+    cout << "Elmers " << Elmers << "\n";
 
     cout << "Process another order [Y] or N: ";
     cin >> RunAgain;
   } while ((RunAgain != "n") && (RunAgain != "N"));
-
 
   return 0; //Yes this is here so it does not show as an error condition on program termination in Windows.
 }
